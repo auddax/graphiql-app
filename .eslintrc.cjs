82c0a75a97e7da0dd7c31a6d690931053fca047f
Kirill Konovalov <auddax@gmail.com>
2023-05-06 16:06:57 +0300

    build: set basic project configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,24 +1,50 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+    parser: '@typescript-eslint/parser',
+    extends: [
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+    ],
+    env: {
+      browser: true,
+      es6: true,
+      jest: true,
+      node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended"
-    ],
-    "overrides": [
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+    root: true,
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-}
+    plugins: ['@typescript-eslint', 'react', 'prettier', 'react-hooks', 'jest-dom'],
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'comma-dangle': ['error', 'only-multiline'],
+      'react/prop-types': 'off',
+      'react/display-name': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      'prettier/prettier': ['error', { endOfLine: 'auto' }],
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/ban-ts-comment': 'error',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+      '@typescript-eslint/no-var-reqiures': 'off',
+      'react/jsx-uses-react': 'off',
+      'react/react-in-jsx-scope': 'off',
+    },
+    settings: {
+      react: {
+        pragma: 'React',
+        version: 'detect',
+      },
+    },
+  };
+  
