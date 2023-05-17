eb0d16d168321a4501912eee64632379cdf86ee0
Александр Демещенко <hell-llex@Aleksandrs-MacBook-Air.local>
2023-05-18 01:56:03 +0300

    feat: add firebase

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import { StoreProvider } from './store/StoreProvider';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <BrowserRouter>
