fc713e6cd10fabd555f13329a901661d065077fe
Александр Демещенко <hell-llex@Aleksandrs-MacBook-Air.local>
2023-05-18 02:00:44 +0300

    feat: add auth store

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,10 +1,14 @@
 import SchemaStore from "./schemaStore";
+import AuthStore from "./authStore";
 
 class RootStore {
   schemaStore: SchemaStore;
+  authStore: AuthStore;
+
   
   constructor() {
     this.schemaStore = new SchemaStore(this);
+    this.authStore = new AuthStore(this);
   }
 }
 
