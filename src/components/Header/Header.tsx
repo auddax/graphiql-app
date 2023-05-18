426694f13d157108def19247ea811640db37cb40
hell-llex <demeshenko333@gmail.com>
2023-05-19 01:20:17 +0300

    feat: add logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import BtnAccount from '../BtnAccount';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className={styles['header']}>
       <div className={styles['header-content']}>
-        Header
-      <BtnAccount showBtn='All' />
+        <NavLink to="/">
+          <img src="./logo.png" className={styles['logo']} />
+        </NavLink>
+        <BtnAccount showBtn="All" />
       </div>
     </div>
   );
