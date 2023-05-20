fda969e1a09ebe6833ae826388d30e87015d223d
Alick Shabanovich <alikshabanovich@gmail.com>
2023-05-20 14:23:56 +0300

    feat: add to 404page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
-import styles from './NotFoundPage.module.scss';
+import React from "react";
+import styles from "./NotFoundPage.module.scss";
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>Not Found Page</div>
+    <div className={styles.wrapper}>
+      <h2 className={styles.heading}>404</h2>
+      <h3 className={styles.subHeading}>
+        Страница не найдена
+      </h3>
+      <Button onClick={() => navigate("/")}>
+        На главную
+      </Button>
+    </div>
   );
 };
 
