c5682075c7ffe8acddb58799f837cd4fe705cec9
Alick Shabanovich <alikshabanovich@gmail.com>
2023-05-26 21:43:16 +0300

    feat: add docs to sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
-import styles from './Sidebar.module.scss';
+import React, { useContext } from 'react';
+import styles from "./Sidebar.module.scss";
+import { StoreContext } from '../../store/StoreProvider';
+import { Spin } from 'antd';
 
 const Sidebar = () => {
+  const store = useContext(StoreContext);
+  const response = store.schemaStore.responseData;
+  const isLoading = store.schemaStore.isLoading;
+  console.log(JSON.stringify(response, null, 4))
+  console.log(response)
+
   return (
-    <></>
+    <>
+      <h2>Docs</h2>
+      <Spin spinning={isLoading} tip="Загрузка...">
+        <pre>{JSON.stringify(response, null, 4)}</pre>
+      </Spin>
+    </>
   );
 };
 
--- src/store/schemaStore.ts
+++ src/store/schemaStore.ts
@@ -1,13 +1,74 @@
-import { makeAutoObservable } from 'mobx';
+import { makeAutoObservable, runInAction } from 'mobx';
 import RootStoreModel from './rootStore';
+import config from '../../config.json';
+import axios from 'axios';
+
+const { headers } = config.api.placeholders;
+
+const query = `{
+  __schema {
+    queryType {
+      fields {
+        name
+        description
+        type {
+          name
+        }
+        args {
+          name
+        }
+      }
+    }
+  }
+}`
 
 class SchemaStore {
   rootStore: RootStoreModel;
+  isLoading: boolean;
+  isError: boolean;
+  errorMessage: string | undefined;
+  queryValue: string | undefined;
+  headersValue: string | undefined;
+  responseData: object | undefined;
 
   constructor(rootStore: RootStoreModel) {
     this.rootStore = rootStore;
     makeAutoObservable(this);
+    this.isLoading = false;
+    this.isError = false;
+    this.queryValue = query;
+    this.headersValue = localStorage.getItem('headers') || headers;
   }
+
+  sendRequest() {
+    this.isLoading = true;
+    this.isError = false;
+    this.errorMessage = '';
+
+    axios.post(
+      config.api.baseUrl,
+      {
+        query: this.queryValue,
+      },
+      {
+        headers: JSON.parse(this.headersValue || '{}'),
+      }
+    ).then(result => {
+        runInAction(() => {
+          this.responseData = result.data;
+          this.isLoading = false;
+        });
+      }
+    ).catch(error => {
+      runInAction(() => {
+        this.responseData = error.response.data;
+        this.isError = true;
+        this.errorMessage = error.message;
+        this.isLoading = false;  
+      });
+    });
+  }
+
 }
 
 export default SchemaStore;
